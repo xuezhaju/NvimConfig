f2205416b4eb56f6cadf4538504e8b34d5c875b0
xuezhaju <liaozecheng123@163.com>
2026-03-15 13:48:15 +0800

    qwq

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -22,30 +22,48 @@ return {
     
     -- LSP 相关
     require("plugins.lsp.init"),
-    require("plugins.lsp.cmp"),  -- 这应该是第25行左右
+    require("plugins.lsp.cmp"),
+
+    -- nvim-navic - 代码导航
+    {
+      "SmiteshP/nvim-navic",
+      dependencies = { "neovim/nvim-lspconfig" },
+      opts = {
+        lsp = {
+          auto_attach = true,
+        },
+      },
+    },  -- ← 这里加了逗号
+
+    -- 撤销树
+    {
+      "XXiaoA/atone.nvim",
+      cmd = "Atone",
+      opts = {},
+    },  -- ← 这里加了逗号
     
     -- 消息通知美化
     {
-    "folke/noice.nvim",
-    event = "VeryLazy",
-    opts = {
-      lsp = {
-        override = {
-          ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
-          ["vim.lsp.util.stylize_markdown"] = true,
+      "folke/noice.nvim",
+      event = "VeryLazy",
+      opts = {
+        lsp = {
+          override = {
+            ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+            ["vim.lsp.util.stylize_markdown"] = true,
+          },
+        },
+        presets = {
+          bottom_search = true,
+          command_palette = true,
+          long_message_to_split = true,
         },
       },
-      presets = {
-        bottom_search = true,
-        command_palette = true,
-        long_message_to_split = true,
+      dependencies = {
+        "MunifTanjim/nui.nvim",
+        "rcarriga/nvim-notify",
       },
-    },
-    dependencies = {
-      "MunifTanjim/nui.nvim",
-      "rcarriga/nvim-notify",
-    }
-    },
+    },  -- ← 这里确保有逗号
 
     -- 标签美化
     {
@@ -70,11 +88,11 @@ return {
         },
         event = "VimEnter",
         config = function()
-            require("plugins.alpha")  -- 我们将在新文件中配置
+            require("plugins.alpha")
         end,
     },
 
-    -- 其他有用插件
+    -- 自动括号
     {
         "windwp/nvim-autopairs",
         event = "InsertEnter",
@@ -83,6 +101,7 @@ return {
         end,
     },
     
+    -- 注释插件
     {
         "numToStr/Comment.nvim",
         keys = {
@@ -94,6 +113,8 @@ return {
         end,
     },
     
+    -- 标签美化（重复了？你这里有两个 bufferline，保留一个即可）
+    -- 如果不需要第二个，可以删除这个
     {
         "akinsho/bufferline.nvim",
         dependencies = "nvim-tree/nvim-web-devicons",
@@ -136,5 +157,5 @@ return {
             { "<S-h>", "<Cmd>BufferLineCyclePrev<CR>", desc = "上一个缓冲区" },
             { "<S-l>", "<Cmd>BufferLineCycleNext<CR>", desc = "下一个缓冲区" },
         },
-    },
+    },  -- ← 最后这个要有逗号吗？如果是最后一个可以不加，但建议统一加
 }
